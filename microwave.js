eb625a4ecec5322163eb93b72a5830187a1b3cb8
James Daniel Holby <james.holby@compuware.com>
2012-05-06 22:47:09 -0400

    Got the next functionality to work all around. Need to get the previous.

---
+++ microwave.js
@@ -0,0 +1,41 @@
+$(document).ready( function() 
+{
+	$("#leftArrow").on("click", function() 
+	{
+		$("figure:visible").fadeOut("slow", function() 
+		{
+			$(this).prev("figure").fadeIn("slow");
+		});
+	});
+	
+	$("#rightArrow").on( "click", function() 
+	{
+		// Check to see if a sibling follows the current <figure/> node
+		if ( $("figure:visible").next("figure").length == 0 )
+		{
+			$("figure:visible").fadeOut("slow", function() 
+			{
+				// Don't forget index starts at 0
+				$("figure").eq(0).fadeIn("slow");
+			});
+		}
+		
+		// If there is a sibling next in line then we can do this
+		else 
+		{
+			$("figure:visible").fadeOut("slow", function() 
+			{			
+				$(this).next().fadeIn("slow");
+				
+				// So we need to turn all the other circles besides this one white
+				$("#folioCircles svg").eq( $(this).index() ).css( "fill", "#FF9F57" );
+				
+				var currentCircle = $("#folioCircles svg").eq( $(this).index() );
+				
+				currentCircle.css( "fill", "#FF9F57" );		
+			});
+		}
+	
+	});
+
+});
